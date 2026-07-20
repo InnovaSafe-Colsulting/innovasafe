9182e90babc8dbbdd077c6ed62ffd8a7a4891088
davidguerrero1011 <davidguerrero0709@gmail.com>
2026-07-20 12:46:42 -0500

    update  2026_07_09_024407_change_modules_columns_to_enum_in_plans_table migration

--- database/migrations/2026_07_09_024407_change_modules_columns_to_enum_in_plans_table.php
+++ database/migrations/2026_07_09_024407_change_modules_columns_to_enum_in_plans_table.php
@@ -7,6 +7,9 @@ return new class extends Migration
 {
     public function up(): void
     {
+        DB::table('plans')->whereNull('basic_modules')->update(['basic_modules' => '1']);
+        DB::table('plans')->whereNull('additional_modules')->update(['additional_modules' => '1']);
+
         DB::statement("ALTER TABLE plans MODIFY basic_modules ENUM('1','0') NOT NULL DEFAULT '1'");
         DB::statement("ALTER TABLE plans MODIFY additional_modules ENUM('1','0') NOT NULL DEFAULT '1'");
     }
